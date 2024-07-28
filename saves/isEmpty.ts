d8605894168167c22c1b6cff132938ec77ed9325
jorgepeniaranda <peniarandajorge@gmail.com>
2024-07-27 22:54:33 -0300

    feat: Add isEmpty function to check if input is empty

---
+++ saves/isEmpty.ts
@@ -0,0 +1,36 @@
+/**
+ * Check if the input is empty
+ * @param {*} input 
+ * @returns {boolean}
+ */
+export function isEmpty(input: unknown): boolean {
+  if (input === undefined || input === null) {
+    return true;
+  }
+
+  if (typeof input === 'boolean' || typeof input === 'function' || typeof input === 'symbol' || typeof input === 'bigint') {
+    return false;
+  }
+
+  if (typeof input === 'number') {
+    return Number.isNaN(input);
+  }
+
+  if (typeof input === 'string') {
+    return input.trim() === '';
+  }
+
+  if (Array.isArray(input)) {
+    return input.length === 0;
+  }
+
+  if (input instanceof Map || input instanceof Set) {
+    return input.size === 0;
+  }
+
+  if (typeof input === 'object') {
+    return Object.keys(input).length === 0;
+  }
+
+  return false;
+}
